68bddfd8a643dc029ebc8c6a8e145426cfee787b
jinkuili666 <115529113+jinkuili666@users.noreply.github.com>
2023-03-29 23:10:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
 function generateExample() {
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.post("/", (req, res) => {
-  const prompt = "请生成一段匹配正则表达式 " + req.body.regex + " 的示例文本：" + req.body.text + "\n";
-  const maxTokens = 1024;
-  const temperature = 0.5;
-  const n = 1;
-  const stop = "\n";
-  const engine = "davinci-codex";
-
-  const request = require("request");
-  const options = {
-    method: "POST",
-    url: "https://api.openai.com/v1/engines/" + engine + "/completions",
-    headers: {
-      Authorization: "Bearer " + process.env.OPENAI_API_KEY,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt,
-      max_tokens: maxTokens,
-      temperature: temperature,
-      n: n,
-      stop: stop,
-    }),
+  var regex = document.getElementById("regex").value;
+  var text = document.getElementById("text").value;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var example = JSON.parse(this.responseText).choices[0].text;
+      document.getElementById("example").innerHTML = example;
+    }
   };
-
-  request(options, (error, response, body) => {
-    if (error) throw new Error(error);
-    res.send(JSON.parse(body).choices[0].text);
-  });
-});
-};
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
-);
+  xhr.open(
+    "POST",
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    true
+  );
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer YOUR_API_KEY");
+  xhr.send(
+    JSON.stringify({
+      prompt: "请生成一段匹配正则表达式 " + regex + " 的示例文本：" + text + "\n",
+      max_tokens: 1024,
+      temperature: 0.5,
+      n: 1,
+      stop: "\n",
+    })
+  );
+}
