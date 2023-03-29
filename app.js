5eb65b30f401671d482114b6bd2d95630bd7eba5
jinkuili666 <115529113+jinkuili666@users.noreply.github.com>
2023-03-29 23:16:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function generateExample() {
     true
   );
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer YOUR_API_KEY");
+  xhr.setRequestHeader("Authorization", "YOUR_API_KEY");
   xhr.send(
     JSON.stringify({
       prompt: "请生成一段匹配正则表达式 " + regex + " 的示例文本：" + text + "\n",
