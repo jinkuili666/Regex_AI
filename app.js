4f72e2c1c224300212ad75bbc26ce2719eb6b5a8
jinkuili666 <115529113+jinkuili666@users.noreply.github.com>
2023-03-30 00:03:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ function generateExample() {
   var regex = document.getElementById("regex").value;
   var text = document.getElementById("text").value;
   var xhr = new XMLHttpRequest();
-  const API_ENDPOINT = "https://vercel.com/jinkuili666/regex-ai/deployments"; 
+  var API_ENDPOINT = "https://vercel.com/jinkuili666/regex-ai/deployments"; 
   if (typeof process === "undefined") {
     var process = {
         env: { NODE_ENV: "production" }
     };
 }
-  const API_KEY = "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";
+  var API_KEY = "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";
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var example = JSON.parse(this.responseText).choices[0].text;
@@ -22,6 +22,7 @@ function generateExample() {
   );
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.setRequestHeader("Authorization", "Bearer ${API_KEY}"); 
+  console.log("Bearer ${API_KEY}");
   xhr.send(
     JSON.stringify({
       prompt: "请生成一段匹配正则表达式 " + regex + " 的示例文本：" + text + "\n",
