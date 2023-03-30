f3ee63ec3543b5ecc65a9936e97c6350ee488e9f
jinkuili666 <115529113+jinkuili666@users.noreply.github.com>
2023-03-30 10:10:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 function generateExample() {
-import('axios').then(axios => {
   var regex = document.getElementById("regex").value;
   var text = document.getElementById("text").value;
   const openai_api_key = process.env.OPENAI_API_KEY;
-
-  const data = {
-    "prompt": "请生成一段匹配正则表达式 " + regex + " 的示例文本：" + text + "\n",
-    "temperature": 0.7,
-    "max_tokens": 60,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0,
-    "stop": "\n"
-  };
-
-  // 创建 HTTP 请求对象并设置 OpenAI API Key
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${openai_api_key}`
+  console.log(openai_api_key);
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var example = JSON.parse(this.responseText).choices[0].text;
+      document.getElementById("example").innerHTML = example;
     }
   };
-
-  // 发送 POST 请求
-  axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', data, config)
-    .then(response => {
-      console.log(response.data);
+  xhr.open(
+    "POST",
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    true
+  );
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + process.env.OPENAI_API_KEY);
+  xhr.send(
+    JSON.stringify({
+      prompt: "请生成一段匹配正则表达式 " + regex + " 的示例文本：" + text + "\n",
+      max_tokens: 1024,
+      temperature: 0.5,
+      n: 1,
+      stop: "\n",
     })
-    .catch(error => {
-      console.log(error);
-    });
-});
-};
+  );
+}
